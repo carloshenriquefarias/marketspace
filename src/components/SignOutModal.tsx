f0449520a0bcf2355bd438357a7a44ebf3d4d78f
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2023-02-03 19:18:49 -0400

    bug

--- src/components/SignOutModal.tsx
+++ src/components/SignOutModal.tsx
@@ -1,40 +1,40 @@
-import React from "react";
-import { Modal, Button, HStack } from "native-base";
+// import React from "react";
+// import { Modal, Button, HStack } from "native-base";
 
-export function SignOutModal() {
-    const [modalVisible, setModalVisible] = React.useState(true);
-    const initialRef = React.useRef(null);
-    const finalRef = React.useRef(null);
-    return <>
-        <Modal isOpen={modalVisible} onClose={() => setModalVisible(false)} initialFocusRef={initialRef} finalFocusRef={finalRef}>
-            <Modal.Content>
-            <Modal.CloseButton />
-            <Modal.Header fontWeight="bold" color="gray.700">ATENÇÃO!</Modal.Header>
+// export function SignOutModal() {
+//     const [modalVisible, setModalVisible] = React.useState(true);
+//     const initialRef = React.useRef(null);
+//     const finalRef = React.useRef(null);
+//     return <>
+//         <Modal isOpen={modalVisible} onClose={() => setModalVisible(false)} initialFocusRef={initialRef} finalFocusRef={finalRef}>
+//             <Modal.Content>
+//             <Modal.CloseButton />
+//             <Modal.Header fontWeight="bold" color="gray.700">ATENÇÃO!</Modal.Header>
 
-            <Modal.Body>
-               Você REALMENTE deseja sair do Marketspace?
-            </Modal.Body>
+//             <Modal.Body>
+//                Você REALMENTE deseja sair do Marketspace?
+//             </Modal.Body>
 
-            <HStack justifyContent="space-between" w="full" p={5} >
-                <Button.Group space={5}>
-                    <Button variant="gray.700" bg="gray.200" colorScheme="blueGray" onPress={() => {
-                        setModalVisible(false);
-                    }}>
-                        Não
-                    </Button>
+//             <HStack justifyContent="space-between" w="full" p={5} >
+//                 <Button.Group space={5}>
+//                     <Button variant="gray.700" bg="gray.200" colorScheme="blueGray" onPress={() => {
+//                         setModalVisible(false);
+//                     }}>
+//                         Não
+//                     </Button>
 
-                    <Button colorScheme="gray.700" bg="gray.700" onPress={() => {
-                        setModalVisible(false);
-                    }}>
-                        Sim
-                    </Button>
-                </Button.Group>
-            </HStack>
+//                     <Button colorScheme="gray.700" bg="gray.700" onPress={() => {
+//                         setModalVisible(false);
+//                     }}>
+//                         Sim
+//                     </Button>
+//                 </Button.Group>
+//             </HStack>
 
-            </Modal.Content>
-        </Modal>
+//             </Modal.Content>
+//         </Modal>
 
        
-    </>;
-}
+//     </>;
+// }
 
