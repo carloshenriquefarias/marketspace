6d576ddf519c74f1b7c867ac634d52393a06faa2
Prisco Cleyton <you@example.comcarloshenriquepvh@hotmail.com>
2023-03-09 20:00:11 -0400

    Configurando BoxCondition e filtros do app

--- src/components/BoxCondition.tsx
+++ src/components/BoxCondition.tsx
@@ -2,36 +2,36 @@ import { Text, Pressable, IPressableProps } from 'native-base';
 
 type Props = IPressableProps & {
   name: string;
-  isActive: boolean
+  isActive: boolean;
 }
 
 export function BoxCondition({ name, isActive, ...rest }: Props) {
   return (
     <Pressable
-        bg="gray.300"
-        rounded={10}
-        h={6}
-        w={'16'} 
-        justifyContent="center"
-        alignItems="center"
-        overflow="hidden"
-        isPressed={isActive}
-        mr={2}
+      bg="gray.300"
+      rounded={10}
+      h={6}
+      w={'16'} 
+      justifyContent="center"
+      alignItems="center"
+      overflow="hidden"
+      isPressed={isActive}
+      mr={2}
 
-        _pressed={{
-            background: 'blue.500'
-        }}
-        
-        {...rest}
+      _pressed={{
+        background: 'blue.500'
+      }}
+      
+      {...rest}
     >
-        <Text
-            color={isActive ? "gray.100" : "gray.500"}
-            textTransform="uppercase"
-            fontSize="xs"
-            fontWeight="bold"
-        >
-            {name}
-        </Text>
+      <Text
+        color={isActive ? "gray.100" : "gray.500"}
+        textTransform="uppercase"
+        fontSize="xs"
+        fontWeight="bold"
+      >
+        {name}
+      </Text>
     </Pressable>
   );
 }
